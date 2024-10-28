e11db4081fb462f97ec9b954ed6546fa6fb36cfc
vorobjovsh <vorobjov75@gmail.com>
2024-10-28 20:16:39 +0200

    start

---
+++ my-slim-app/app/routes.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Application\Actions\User\ListUsersAction;
+use App\Application\Actions\User\ViewUserAction;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\App;
+use Slim\Interfaces\RouteCollectorProxyInterface as Group;
+use Slim\Views\Twig;
+//use Slim\Views\TwigMiddleware;
+
+return function (App $app) {
+    $app->options('/{routes:.*}', function (Request $request, Response $response) {
+        // CORS Pre-Flight OPTIONS Request Handler
+        return $response;
+    });
+
+    /* $app->get('/', function (Request $request, Response $response) {
+        $response->getBody()->write('Hello world!');
+        return $response;
+    }); */
+	
+	$app->get('/', function ($request, $response) {
+		$view = Twig::fromRequest($request);
+		
+		return $view->render($response, 'home.html.twig', [
+			'name' => 'John Doe Junior',
+		]);
+	});
+
+    $app->group('/users', function (Group $group) {
+        $group->get('', ListUsersAction::class);
+        $group->get('/{id}', ViewUserAction::class);
+    });
+};
